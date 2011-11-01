bac6670f9a307d79c716c8b69cfebf91ef6b8c46
IamPersistent <develop@zestic.com>
2011-11-01 01:45:21 -0700

    add initial model classes

---
+++ Model/Inventory.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * (c) Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\InventoryBundle\Model;
+
+use Vespolina\InventoryBundle\Model\InventoryInterface;
+
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+abstract class Inventory implements InventoryInterface
+{
+
+}
